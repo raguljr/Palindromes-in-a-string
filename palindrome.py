bbdb1865e236edcf04996887c08034a8e911d045
Ragul Jothiraj <jothiraj.ragul@gmail.com>
2016-08-07 21:29:25 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+l=raw_input()
+co=0
+for i in range(len(l)+1):
+	for j in range(len(l)+1):
+		if i>j:
+			t=l[j:i]
+			if t==t[::-1]:
+				#print t  #this statement prints unique substring palindromes
+				co=co+1
+print co
+
+			
